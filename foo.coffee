6ffbefda846d869b65406703d436a269f75642d6
Yesudeep Mangalapilly <gora.khargosh@gmail.com>
2011-03-02 21:37:31 +0530

    Getting view to work
    Signed-off-by: Yesudeep Mangalapilly <gora.khargosh@gmail.com>

--- foo.coffee
+++ foo.coffee
@@ -1,21 +1,41 @@
 
 
 $(() ->
+    class ButtonView extends Backbone.View
+        tagName: 'div'
+        
+        id: 'foobar'
+        
+        events: {
+            'click #whatever': 'onButtonClicked'
+        }
+        
+        constructor: () ->
+            super()
+            console.log('View constructor called')
+            console.log($(@el).html())
+            
+        onButtonClicked: () =>
+            console.log('whatever clicked')
+            return
+            
+        render: (event) =>
+            console.log("something to render")
+            return this
+
     class ApplicationController extends Backbone.Controller
         routes: {
             'help': 'help',
-            'search/:bloodType/:latitude/:longitude': "search",
         }
 
         constructor: () ->
-            console.log "constructor called"
+            super()
+            console.log "ApplicationController constructor called"
+            @buttonView = new ButtonView()
             return
 
         help: () -> 
             console.log('help called')
-
-        search: (bloodType, latitude, longitude) ->
-            return
 
     new ApplicationController()
     Backbone.history.start()
--- foo.js
+++ foo.js
@@ -1,5 +1,5 @@
 (function() {
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
     ctor.prototype = parent.prototype;
@@ -8,21 +8,43 @@
     return child;
   };
   $(function() {
-    var ApplicationController;
+    var ApplicationController, ButtonView;
+    ButtonView = (function() {
+      __extends(ButtonView, Backbone.View);
+      ButtonView.prototype.tagName = 'div';
+      ButtonView.prototype.id = 'foobar';
+      ButtonView.prototype.events = {
+        'click #whatever': 'onButtonClicked'
+      };
+      function ButtonView() {
+        this.render = __bind(this.render, this);;
+        this.onButtonClicked = __bind(this.onButtonClicked, this);;        ButtonView.__super__.constructor.call(this);
+        console.log('View constructor called');
+        console.log($(this.el).html());
+      }
+      ButtonView.prototype.onButtonClicked = function() {
+        console.log('whatever clicked');
+      };
+      ButtonView.prototype.render = function(event) {
+        console.log("something to render");
+        return this;
+      };
+      return ButtonView;
+    })();
     ApplicationController = (function() {
       __extends(ApplicationController, Backbone.Controller);
       ApplicationController.prototype.routes = {
-        'help': 'help',
-        'search/:bloodType/:latitude/:longitude': "search"
+        'help': 'help'
       };
       function ApplicationController() {
-        console.log("constructor called");
+        ApplicationController.__super__.constructor.call(this);
+        console.log("ApplicationController constructor called");
+        this.buttonView = new ButtonView();
         return;
       }
       ApplicationController.prototype.help = function() {
         return console.log('help called');
       };
-      ApplicationController.prototype.search = function(bloodType, latitude, longitude) {};
       return ApplicationController;
     })();
     new ApplicationController();
